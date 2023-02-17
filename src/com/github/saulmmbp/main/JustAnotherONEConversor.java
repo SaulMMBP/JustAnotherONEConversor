16300f60ec4110930a16d5453f3568aca3f36dc8
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-02-17 13:01:53 -0600

    Inicial

---
+++ src/com/github/saulmmbp/main/JustAnotherONEConversor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.github.saulmmbp.main;
+
+/**
+ * Conversor de:
+ * - Monedas
+ * @author SAUL MALAGON MARTINEZ
+ *
+ */
+public class JustAnotherONEConversor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
