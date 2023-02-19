f0168b375172e4836078536ccbd2d8dc2c82402a
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-02-18 18:11:41 -0600

    Se agregaron tipos de conversión

---
+++ src/com/github/saulmmbp/main/conversion/types/ConversionType.java
@@ -0,0 +1,12 @@
+package com.github.saulmmbp.main.conversion.types;
+
+/**
+ * Tipos de conversión que soporta el convertidor
+ * @author SAUL MALAGON MARTINEZ
+ *
+ */
+public enum ConversionType {
+
+    CURRENCY,
+    TEMPERATURE
+}
