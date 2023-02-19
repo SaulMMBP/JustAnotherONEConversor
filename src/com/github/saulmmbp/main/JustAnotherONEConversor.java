fe7825b795aa4b404b64ea6e32b0edae70fb33c6
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-02-18 18:11:18 -0600

    Se modificó el look and feel

--- src/com/github/saulmmbp/main/JustAnotherONEConversor.java
+++ src/com/github/saulmmbp/main/JustAnotherONEConversor.java
@@ -3,6 +3,13 @@
  */
 package com.github.saulmmbp.main;
 
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+import javax.swing.*;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import com.github.saulmmbp.main.ui.Window;
+
 /**
  * Conversor de:
  * - Monedas
@@ -10,12 +17,35 @@ package com.github.saulmmbp.main;
  *
  */
 public class JustAnotherONEConversor {
+    
+    private static Window window;
 
 	/**
+	 * Método main
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+	    init();
+	}
+	
+	/**
+	 * Inicializa la ventana principal
+	 */
+	private static void init() {
+	    try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println("No se pudo cargar el Look and Feel, se usará el Look And Feel por defecto");
+            e.printStackTrace();
+        }
+	    
+	    
+	    window = new Window();
+	    window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    window.setSize(480, 320);
+	    window.setResizable(false);
+	    window.setTitle("Just Another ONE Conversor");
+	    window.setVisible(true);
 	}
 
 }
