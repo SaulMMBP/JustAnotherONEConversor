701d529ab89d438b88efaae835b3078a4edfd3c5
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-02-18 19:16:44 -0600

    Modifiqué el método de conversión, ahora es más sencillo

--- src/com/github/saulmmbp/main/conversion/CurrencyConversor.java
+++ src/com/github/saulmmbp/main/conversion/CurrencyConversor.java
@@ -23,69 +23,18 @@ public class CurrencyConversor implements Conversor {
         }
     }
     
-    private float conversion(float cantidad, Currency from, Currency to) {
-        String searchString1 = from.name() + "_" + to.name();
-        String searchString2 = to.name() + "_" + from.name();
-        if(properties.containsKey(searchString1)) {
-            return cantidad * Float.parseFloat(properties.getProperty(searchString1));
-        } else {
-            return cantidad / Float.parseFloat(properties.getProperty(searchString2));
-        }
-    }
-    
     @Override
     public float convert(float cantidad, Convertible from, Convertible to) {
         Currency localFrom = (Currency) from;
         Currency localTo = (Currency) to;
-        return switch (localFrom) {
-            case PESO -> switch (localTo) {
-                case PESO -> cantidad;
-                case DOLAR -> conversion(cantidad, localFrom, localTo);
-                case EURO -> conversion(cantidad, localFrom, localTo);
-                case LIBRA_ESTERLINA -> conversion(cantidad, localFrom, localTo);
-                case YEN_JAPONES -> conversion(cantidad, localFrom, localTo);
-                case WON_SURCOREANO -> conversion(cantidad, localFrom, localTo);
-            };
-            case DOLAR -> switch (localTo) {
-                case PESO -> conversion(cantidad, localFrom, localTo);
-                case DOLAR -> cantidad;
-                case EURO -> conversion(cantidad, localFrom, localTo);
-                case LIBRA_ESTERLINA -> conversion(cantidad, localFrom, localTo);
-                case YEN_JAPONES -> conversion(cantidad, localFrom, localTo);
-                case WON_SURCOREANO -> conversion(cantidad, localFrom, localTo);
-            };
-            case EURO -> switch (localTo) {
-                case PESO -> conversion(cantidad, localFrom, localTo);
-                case DOLAR -> conversion(cantidad, localFrom, localTo);
-                case EURO -> cantidad;
-                case LIBRA_ESTERLINA -> conversion(cantidad, localFrom, localTo);
-                case YEN_JAPONES -> conversion(cantidad, localFrom, localTo);
-                case WON_SURCOREANO -> conversion(cantidad, localFrom, localTo);
-            };
-            case LIBRA_ESTERLINA -> switch (localTo) {
-                case PESO -> conversion(cantidad, localFrom, localTo);
-                case DOLAR -> conversion(cantidad, localFrom, localTo);
-                case EURO -> conversion(cantidad, localFrom, localTo);
-                case LIBRA_ESTERLINA -> cantidad;
-                case YEN_JAPONES -> conversion(cantidad, localFrom, localTo);
-                case WON_SURCOREANO -> conversion(cantidad, localFrom, localTo);
-            };
-            case YEN_JAPONES -> switch (localTo) {
-                case PESO -> conversion(cantidad, localFrom, localTo);
-                case DOLAR -> conversion(cantidad, localFrom, localTo);
-                case EURO -> conversion(cantidad, localFrom, localTo);
-                case LIBRA_ESTERLINA -> conversion(cantidad, localFrom, localTo);
-                case YEN_JAPONES -> cantidad;
-                case WON_SURCOREANO -> conversion(cantidad, localFrom, localTo);
-            };
-            case WON_SURCOREANO -> switch (localTo) {
-                case PESO -> conversion(cantidad, localFrom, localTo);
-                case DOLAR -> conversion(cantidad, localFrom, localTo);
-                case EURO -> conversion(cantidad, localFrom, localTo);
-                case LIBRA_ESTERLINA -> conversion(cantidad, localFrom, localTo);
-                case YEN_JAPONES -> conversion(cantidad, localFrom, localTo);
-                case WON_SURCOREANO -> cantidad;
-            };
-        };
+        String searchString1 = localFrom.name() + "_" + localTo.name();
+        String searchString2 = localTo.name() + "_" + localFrom.name();
+        if(properties.containsKey(searchString1)) {
+            return cantidad * Float.parseFloat(properties.getProperty(searchString1));
+        } else if(properties.containsKey(searchString2)) {
+            return cantidad / Float.parseFloat(properties.getProperty(searchString2));
+        } else {
+            return cantidad;
+        }
     }
 }
