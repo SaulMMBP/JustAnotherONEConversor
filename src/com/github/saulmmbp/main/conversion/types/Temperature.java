c72dfb548bdda60f4032cb95497b11298e2cddbf
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-02-18 18:13:12 -0600

    Se agregaron las unidades de temperatura

---
+++ src/com/github/saulmmbp/main/conversion/types/Temperature.java
@@ -0,0 +1,13 @@
+package com.github.saulmmbp.main.conversion.types;
+
+/**
+ * Unidades de temperatura
+ * @author SAUL MALAGON MARTINEZ
+ *
+ */
+public enum Temperature implements Convertible {
+
+    CELSIUS, 
+    FAHRENHEIT, 
+    KELVIN
+}
