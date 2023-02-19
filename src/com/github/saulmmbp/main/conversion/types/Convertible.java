36b61e2a031554d6e8e38974dc376952ac482071
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-02-18 18:12:44 -0600

    Se agregó la interfaz convertible para poder generalizar conversiones

---
+++ src/com/github/saulmmbp/main/conversion/types/Convertible.java
@@ -0,0 +1,10 @@
+package com.github.saulmmbp.main.conversion.types;
+
+/**
+ * Agrupa las unidades de conversión en el tipo Convertible
+ * @author SAUL MALAGON MARTINEZ
+ *
+ */
+public interface Convertible {
+
+}
